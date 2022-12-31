23d150299f90317d6bb17716f24167d7b181b749
Jonn Alves <jonatas.alsantos@jsl.com.br>
2022-12-30 22:55:53 -0300

    Initial Commit

---
+++ flutter.ios/flutter_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_iosApp.swift
+//  flutter.ios
+//
+//  Created by Jonn Alves on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct flutter_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
